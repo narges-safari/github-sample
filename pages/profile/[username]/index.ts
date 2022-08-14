b271563159ad1f8a45b2d1194cf85600b44739d8
narges safari <narges.safari.8631@gmail.com>
2022-08-14 16:44:16 +0430

    feat use Promise.all to fetch to requests

--- pages/profile/[username]/index.ts
+++ pages/profile/[username]/index.ts
@@ -2,15 +2,19 @@ export { default } from "./Profile";
 import { GetServerSideProps } from "next";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const res = await fetch(
-    `https://api.github.com/users/${context.params?.username}`
-  );
+  const [userRes, reposRes] = await Promise.all([
+    fetch(`https://api.github.com/users/${context.params?.username}`),
+    fetch(`https://api.github.com/users/${context.params?.username}/repos`),
+  ]);
 
-  const user = await res.json();
+  const [user, repos] = await Promise.all([userRes.json(), reposRes.json()]);
+
+  console.log("repos", repos);
 
   return {
     props: {
       user,
+      repos,
     },
   };
 };
